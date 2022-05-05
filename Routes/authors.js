90fc3e5edb78873d7dc8e37b7d14de45b25a94e1
“de-marauder” <ezikegodson@gmail.com>
2022-05-05 19:42:22 +0100

    author create and search functionality

--- Routes/authors.js
+++ Routes/authors.js
@@ -1,18 +1,45 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res)=>{
-    console.log("authout route hit")
-    res.render('Authors/authors')
-})
-router.get('/new', (req, res)=>{
-    console.log("authout route hit")
-    res.render('Authors/new')
-})
-router.post('/', (req, res)=>{
-    console.log("author route hit")
-    res.send('Create')
-})
+const Author = require("../models/author");
 
-module.exports = router
+// Author routes
+router.get("/", async (req, res) => {
+  console.log("author route hit");
+  const name = req.query.name
+  let query = {}
+  if (name) query.name = new RegExp(name, 'i')
+  try {
+    const authors = await Author.find(query);
+    res.render("Authors", { authors: authors, search: name });
+  } catch (error) {
+      console.log(error)
+      res.redirect('/')
+  }
+});
+router.get("/new", (req, res) => {
+  console.log(Author);
+  console.log("author route hit");
+  res.render("Authors/new", { author: new Author() });
+});
+router.post("/", async (req, res) => {
+  console.log("author route hit");
+
+  const author = new Author({
+    name: req.body.name,
+  });
+
+  try {
+    const newAuthor = await author.save();
+    res.redirect(`Authors`);
+  } catch (error) {
+    console.log("there was an error");
+    res.render("Authors/new", {
+      author: author,
+      errorMessage: `Error creating Author`,
+    });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const express = require('express');
+const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 const indexRoute = require('./Routes')
 const authorRoute = require('./Routes/authors.js')
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
+app.use(bodyParser.urlencoded({extended: false, limit: '10mb'}))
 app.use(expressLayouts)
 app.use(express.static('public'))
 
