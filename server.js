e205397cc47014d42c435da5c35f306128cf9998
“de-marauder” <ezikegodson@gmail.com>
2022-05-05 17:12:41 +0100

    update

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 const indexRoute = require('./Routes')
+const authorRoute = require('./Routes/authors.js')
+
 const app = express()
 
 const mongoose = require('mongoose')
+console.log('start connecting to database...')
 mongoose.connect(process.env.DATABASE_URL)
+console.log('connected to database...')
 
 const db = mongoose.connection
 db.on('error', error=>console.error(error))
@@ -22,9 +26,12 @@ app.use(express.static('public'))
 
 const port = process.env.PORT || 5000
 
-app.get('/', indexRoute)
-// app.get('/author', authorRoute)
+app.use('/', indexRoute)
+app.use('/authors', authorRoute)
 
+app.get('/*', (req, res)=>{
+    res.send("Not Found")
+})
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
